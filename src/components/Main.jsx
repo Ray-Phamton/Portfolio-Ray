9f3cc1a24c083e04fd786918ed3fbc0ec0e33a3f
Ray-Phamton <raymonreyesxddd@gmail.com>
2025-11-08 00:49:39 -0400

    fix: The framer-motion import was fixed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,22 +4,22 @@ import { StudySection } from './MainComponents/StudySection'
 import { ToolsSection } from './MainComponents/ToolsSection'
 import { ExperienceSection } from './MainComponents/ExperienceSection'
 import { useState, useEffect } from 'react'
-import { motion } from 'framer-motion'
+import { motion } from "motion/react"
 import { TiArrowUpThick } from 'react-icons/ti'
 
 export function Main () {
   const [isVisible, setIsVisible] = useState(false)
 
-  // Función para manejar el evento de scroll
+  // Function to handle the scroll event
   const toggleVisibility = () => {
-    if (window.scrollY > 100) { // Muestra el botón si el scroll es mayor a 300px
+    if (window.scrollY > 100) { // Show the button if the scroll is greater than 300px
       setIsVisible(true)
     } else {
       setIsVisible(false)
     }
   }
 
-  // Agrega y elimina el listener de scroll
+  // Add and remove the scroll listener
   useEffect(() => {
     window.addEventListener('scroll', toggleVisibility)
     return () => {
@@ -47,18 +47,16 @@ export function Main () {
 
         <motion.button
           onClick={handleScrollToTop}
-      // **Framer Motion**: Define cómo entra y sale el botón
-          initial={{ opacity: 0, scale: 0.7 }} // Estado inicial (oculto)
-          animate={{ opacity: isVisible ? 1 : 0, scale: isVisible ? 1 : 0.5 }} // Animar basado en 'isVisible'
-          transition={{ duration: 0.3 }} // Duración de la animación
-      // Opcional: Animar al hacer hover (interactividad)
+          initial={{ opacity: 0, scale: 0.7 }}
+          animate={{ opacity: isVisible ? 1 : 0, scale: isVisible ? 1 : 0.5 }}
+     
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.9 }}
-      // Estilos para la posición fija
+      
           className='fixed border-none flex items-center justify-center bg-radial-[at_25%_80%] from-violet-700 via-75% to-black text-gray-100 rounded-full shadow-xl shadow-violet-700/50 hover:shadow-violet-700 transition duration-1000 size-16 sm:size-20 right-10 bottom-20 sm:bottom-14'
           style={{
-            // ... otros estilos ...
-            pointerEvents: isVisible ? 'auto' : 'none' // Deshabilita clics si está oculto
+
+            pointerEvents: isVisible ? 'auto' : 'none'
           }}
         >
           <TiArrowUpThick className=' text-gray-100 sm:size-18 size-14' />
--- src/components/MainComponents/HeroSection.jsx
+++ src/components/MainComponents/HeroSection.jsx
@@ -10,7 +10,7 @@ export function HeroSection () {
 
       <section className='flex flex-col sm:flex-row lg:pl-32 md:pl-20 sm:pl-12  h-full z-10 pt-20'>
         <section className='flex flex-col justify-center sm:items-stretch sm:w-1/2 items-center h-full gap-2 z-10'>
-          <img className=' sm:hidden h-52 mask-radial-at-center mask-radial-from-58% mask-radial-to-75% rounded-full object-cover mt-6' src='../../../public/Raymon-perfil.jpeg' alt='image of Raymon Reyes' />
+          <img className=' sm:hidden h-52 mask-radial-at-center mask-radial-from-58% mask-radial-to-75% rounded-full object-cover mt-6' src='/Raymon-perfil.jpeg' alt='image of Raymon Reyes' />
           <h1 className='lg:text-6xl md:text-5xl text-4xl font-extrabold sm:mb-4 text-gray-100'>Raymon Reyes</h1>
           <div className='flex sm:hidden items-center h-1/5 gap-4 pt-6'>
             <SocialBubbles />
@@ -22,7 +22,7 @@ export function HeroSection () {
         </section>
 
         <section className='hidden sm:flex flex-col justify-center items-center h-full w-1/2 z-10'>
-          <img className=' lg:h-80 md:h-72 sm:h-60 h-52 mask-radial-at-center mask-radial-from-58% mask-radial-to-75% rounded-full object-cover' src='../../../public/Raymon-perfil.jpeg' alt='image of Raymon Reyes' />
+          <img className=' lg:h-80 md:h-72 sm:h-60 h-52 mask-radial-at-center mask-radial-from-58% mask-radial-to-75% rounded-full object-cover' src='/Raymon-perfil.jpeg' alt='image of Raymon Reyes' />
           <div className='flex items-center h-1/5 md:gap-8 sm:gap-2 gap-4 pt-24'>
             <SocialBubbles />
           </div>
